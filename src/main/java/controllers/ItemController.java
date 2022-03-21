11f2a8a1811ac141ec6c3a1515df27e61e347c65
looper <172508294@qq.com>
2022-03-21 13:47:36 +0800

    接口定义

---
+++ src/main/java/controllers/ItemController.java
@@ -0,0 +1,50 @@
+package controllers;
+
+import controllers.dao.User;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class ItemController {
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label priceLable;
+
+    @FXML
+    private ImageView img;
+
+
+
+    @FXML
+    private void click(MouseEvent mouseEvent) {
+        myListener.onClickListener(users);
+    }
+
+    private User users;
+    private MyListener myListener;
+
+
+
+    public void setData(User user, MyListener myListener) {
+        this.users = user;
+        this.myListener = myListener;
+        nameLabel.setText(user.getName());
+        priceLable.setText(user.getIp());
+        Image image = new Image(getClass().getResourceAsStream("/images/computer_green.png"));
+        img.setImage(image);
+    }
+}
---
+++ src/main/java/controllers/MyListener.java
@@ -0,0 +1,10 @@
+package controllers;
+
+
+import controllers.dao.User;
+
+import java.util.List;
+
+public interface MyListener {
+    public void onClickListener(User users);
+}
