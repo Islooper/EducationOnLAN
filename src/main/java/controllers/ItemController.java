1504a75464aaa79de26db432233d7a593c161c05
looper <172508294@qq.com>
2022-04-02 11:00:33 +0800

    布局问题

--- src/main/java/controllers/ItemController.java
+++ src/main/java/controllers/ItemController.java
@@ -10,14 +10,17 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
-public class ItemController {
+public class ItemController implements Initializable {
     @FXML
     private Label nameLabel;
 
@@ -27,6 +30,9 @@ public class ItemController {
     @FXML
     private ImageView img;
 
+
+    @FXML
+    private AnchorPane ap;
 
 
     @FXML
@@ -42,10 +48,16 @@ public class ItemController {
     public void setData(User user , MyListener myListener) {
         this.users = user;
         this.myListener = myListener;
-        nameLabel.setText(user.getName());
+        nameLabel.setText("小明");
         priceLable.setText(user.getIp());
 //        Image image = new Image(getClass().getResourceAsStream("/images/computer_green.png"));
-        Image image = new Image("file:/images/computer_green.png");
+        File file = new File("images/computer_green.png");
+        String string = file.toURI().toString();
+        Image image = new Image(string);
         img.setImage(image);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
     }
 }
